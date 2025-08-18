a77f2870cbff00099d415e5ab3256899394bb9d8
KelloHitt <venus.cert@gmail.com>
2025-08-18 18:10:18 +1200

    Returned a new array which does not contain elements that are meant to be removed

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
 const removeFromArray = function (array, ...args) {
     console.log(array);
     console.log(args);
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        if (!(args.includes(array[i]))) {
+            newArray.push(array[i]);
+        }
+    }
+    console.log(newArray);
+    return newArray;
 };
 
 // Do not edit below this line
