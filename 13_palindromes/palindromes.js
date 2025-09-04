a79e708a5b29477659ca3982148fd82754553771
KelloHitt <venus.cert@gmail.com>
2025-09-05 09:08:34 +1200

    Added implementation for finding a palindrome by getting rid of punctuation and reversing the string

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newString = string.toLowerCase();
+    let noPunct = "";
+    earliestNum = String(0);
+    lastNum = String(9);
+    for (let i = 0; i < string.length; i++) {
+        if ((string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 122) || (string.charCodeAt(i) >= earliestNum.charCodeAt(0) && (string.charCodeAt(i) >= lastNum.charCodeAt(0))));
+        noPunct += string[i];
+        console.log("String: ", string[i]);
+    }
+    console.log("No punctuation: ", noPunct);
+    let reversedArray = noPunct.split('');
+    reversedArray.reverse();
+    let reversedString = reversedArray.join('');
+    console.log('reversed string: ', reversedString);
+    return noPunct == reversedString;
 
 };
 
