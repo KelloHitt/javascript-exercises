2f6c1d1bfbce24762b17b496ac85fb50197b4819
KelloHitt <venus.cert@gmail.com>
2025-09-08 09:30:15 +1200

    Checked if the character is not a empty space and checked that only alphanumeric values are present

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,18 +1,15 @@
 const palindromes = function (string) {
-    let newString = string.toLowerCase();
     let noPunct = "";
-    earliestNum = String(0);
-    lastNum = String(9);
     for (let i = 0; i < string.length; i++) {
-        if ((string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 122) || (string.charCodeAt(i) >= earliestNum.charCodeAt(0) && (string.charCodeAt(i) >= lastNum.charCodeAt(0))));
-        noPunct += string[i];
-        console.log("String: ", string[i]);
+        if ((string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 122) || (!isNaN(Number(string[i]))) && string[i] != " ") {
+            noPunct += string[i].toLowerCase();
+            console.log("String: ", string[i]);
+        }
+
     }
-    console.log("No punctuation: ", noPunct);
     let reversedArray = noPunct.split('');
     reversedArray.reverse();
     let reversedString = reversedArray.join('');
-    console.log('reversed string: ', reversedString);
     return noPunct == reversedString;
 
 };
