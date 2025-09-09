1339b4935b58af0f19c171af21497ef8103f01e9
KelloHitt <venus.cert@gmail.com>
2025-09-10 07:43:25 +1200

    Iterated through each object to calculate the person's agethen checked to see whether their age was the oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function (peopleArray) {
+    let age = 0;
+    let oldestObject = {};
+    for (const obj of peopleArray) {
+        let oldness = 0;
+        if ('yearOfDeath' in obj) {
+            oldness = obj.yearOfDeath - obj.yearOfBirth;
+        } else {
+            const currentDate = new Date();
+            oldness = currentDate.getFullYear() - obj.yearOfBirth;
+            console.log("Seeing age: ", obj.name, oldness);
+        }
+        if (oldness > age) {
+            age = oldness;
+            oldestObject = obj;
+        }
+    }
+    return oldestObject;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
